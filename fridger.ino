b37e52838fd1c439e2835f5dd1c2efca0a7b81c6
Francis Duvivier <francisduvivier@gmail.com>
2022-06-25 03:10:58 +0200

    Add OTA code

--- fridger.ino
+++ fridger.ino
@@ -8,18 +8,58 @@ float onTemp = 7.5;
 #define DHTTYPE DHT22
 
 DHT dht(DHTPIN, DHTTYPE);
+#include <ESP8266WiFi.h>
+#include <ESP8266mDNS.h>
+#include <WiFiUdp.h>
+#include <ArduinoOTA.h>
+
+// Replace with your network credentials
+const char* ssid = "YOUR_SSID";
+const char* password = "YOUR_PASSWORD";
+
+const int ESP_BUILTIN_LED = 2;
+
 
 void setup(void) {
   // preparing GPIOs
   Serial.begin(115200);
+  Serial.println("Booting");
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
   dht.begin();
   pinMode(gpio13Led, OUTPUT);
   pinMode(gpio12Relay, OUTPUT);
   digitalWrite(gpio13Led, LOW);
   digitalWrite(gpio12Relay, HIGH);
+  if (WiFi.waitForConnectResult() != WL_CONNECTED) {
+    Serial.println("Connection Failed! No OTA Possible...");
+    return;
+  }
+  ArduinoOTA.onStart([]() {
+    Serial.println("Start");
+  });
+  ArduinoOTA.onEnd([]() {
+    Serial.println("\nEnd");
+  });
+  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+  });
+  ArduinoOTA.onError([](ota_error_t error) {
+    Serial.printf("Error[%u]: ", error);
+    if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
+    else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
+    else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
+    else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
+    else if (error == OTA_END_ERROR) Serial.println("End Failed");
+  });
+  ArduinoOTA.begin();
+  Serial.println("Ready");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
 }
 
 void loop(void) {
+  ArduinoOTA.handle();
   float temp = dht.readTemperature(false);
   Serial.println("temp");
 
@@ -39,5 +79,4 @@ void loop(void) {
     digitalWrite(gpio13Led, LOW);
     digitalWrite(gpio12Relay, HIGH);
   }
-  delay(1000);
 }
