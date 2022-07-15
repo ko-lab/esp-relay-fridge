576d070c4716ae9b9078fe1da7367b9851084d03
Francis Duvivier <francisduvivier@gmail.com>
2022-07-15 19:44:02 +0200

    Add coolingOnOff mqtt message
    And also Add blink function, decrease delays

--- fridger.ino
+++ fridger.ino
@@ -5,11 +5,10 @@ int FAN_RELAY_PIN = 16;
 float offTemp = 6;
 float extraDegreesForFan = 0.4; // offtemp+extraDegreesForFan*2 should always be smaller than onTemp
 boolean ON_STATE = 1;
+boolean COOLING_ON_STATE = 0;
 float onTemp = 9;
 boolean coolingOn = false;
-//assert(extraDegreesForFan > 0);
-//assert(offTemp < onTemp);
-//assert(offTemp + 2 * extraDegreesForFan < onTemp);
+
 #include "DHT.h"
 #define DHTPIN 5
 #define DHTTYPE DHT22
@@ -28,9 +27,11 @@ DHT dht(DHTPIN, DHTTYPE);
 WiFiClient client;
 Adafruit_MQTT_Client mqtt(&client, AIO_SERVER, AIO_SERVERPORT);
 Adafruit_MQTT_Publish fridge = Adafruit_MQTT_Publish(&mqtt, "/feeds/fridge/appliance_temp");
-Adafruit_MQTT_Publish fridge_state = Adafruit_MQTT_Publish(&mqtt, "/feeds/fridge/state");
+Adafruit_MQTT_Publish fridge_state = Adafruit_MQTT_Publish(&mqtt, "feeds/fridge/state");
+Adafruit_MQTT_Publish cooling_state = Adafruit_MQTT_Publish(&mqtt, "feeds/fridge/cooling_state");
 
-Adafruit_MQTT_Subscribe onoffbutton = Adafruit_MQTT_Subscribe(&mqtt, "/feeds/fridge/onoff");
+Adafruit_MQTT_Subscribe coolingOnOffButton = Adafruit_MQTT_Subscribe(&mqtt, "feeds/fridge/cooling_onoff");
+Adafruit_MQTT_Subscribe onoffbutton = Adafruit_MQTT_Subscribe(&mqtt, "feeds/fridge/onoff");
 
 // Replace with your network credentials
 const char* ssid = "ko-lab-iot";
@@ -42,6 +43,15 @@ const int ESP_BUILTIN_LED = 2;
 // for some reason (only affects ESP8266, likely an arduino-builder bug).
 void MQTT_connect();
 unsigned long lastPublish = 0;
+void blink(int delayMillis, int times) {
+  for (int i = 0; i < times; i++) {
+    digitalWrite(LED_PIN, HIGH);
+    delay(delayMillis);
+    digitalWrite(LED_PIN, LOW);
+    delay(delayMillis);
+  }
+}
+
 void setup(void) {
   pinMode(LED_PIN, OUTPUT);
   pinMode(COOLING_RELAY_PIN, OUTPUT);
@@ -56,31 +66,12 @@ void setup(void) {
   WiFi.begin(ssid, password);
   dht.begin();
 
-  delay(500);
-  digitalWrite(LED_PIN, HIGH);
-  delay(500);
-  digitalWrite(LED_PIN, LOW);
-  delay(500);
-  digitalWrite(LED_PIN, HIGH);
-  delay(500);
-  digitalWrite(LED_PIN, LOW);
+  blink(500, 2);
   digitalWrite(FAN_RELAY_PIN, HIGH);
 
   if (WiFi.waitForConnectResult() != WL_CONNECTED) {
     Serial.println("Connection Failed! No OTA Possible...");
-    digitalWrite(LED_PIN, HIGH);
-    delay(200);
-    digitalWrite(LED_PIN, LOW);
-    delay(200);
-    digitalWrite(LED_PIN, HIGH);
-    delay(200);
-    digitalWrite(LED_PIN, LOW);
-    delay(200);
-    digitalWrite(LED_PIN, HIGH);
-    delay(200);
-    digitalWrite(LED_PIN, LOW);
-    delay(200);
-    digitalWrite(LED_PIN, HIGH);
+    blink(500, 4);
     digitalWrite(COOLING_RELAY_PIN, LOW);
     return;
   }
@@ -102,19 +93,16 @@ void setup(void) {
     else if (error == OTA_END_ERROR) Serial.println("End Failed");
   });
   ArduinoOTA.begin();
+  blink(500, 1);
   Serial.println("Ready");
   Serial.print("IP address: ");
   Serial.println(WiFi.localIP());
   mqtt.subscribe(&onoffbutton);
-  delay(500);
-  digitalWrite(LED_PIN, HIGH);
-  delay(500);
-  digitalWrite(LED_PIN, LOW);
-  delay(500);
-  digitalWrite(LED_PIN, HIGH);
-  delay(500);
-  digitalWrite(LED_PIN, LOW);
-  digitalWrite(LED_PIN, ON_STATE? HIGH: LOW);
+  mqtt.subscribe(&coolingOnOffButton);
+
+  blink(500, 1);
+  MQTT_connect();
+  blink(300, 3);
 
 }
 
@@ -162,11 +150,15 @@ void handleMQTT(float temp) {
   // try to spend your time here
 
   Adafruit_MQTT_Subscribe *subscription;
-  if((subscription = mqtt.readSubscription(100))) {
-    if (subscription == &onoffbutton) {
-        ON_STATE = !ON_STATE;
-        digitalWrite(LED_PIN, ON_STATE? HIGH: LOW);
-        fridge_state.publish(ON_STATE? "HIGH": "LOW");
+  if ((subscription = mqtt.readSubscription(100))) {
+    if (subscription == &coolingOnOffButton) {
+      COOLING_ON_STATE = !COOLING_ON_STATE;
+      digitalWrite(COOLING_RELAY_PIN, COOLING_ON_STATE ? HIGH : LOW);
+      cooling_state.publish(COOLING_ON_STATE ? "HIGH" : "LOW");
+    } else     if (subscription == &onoffbutton) {
+      ON_STATE = !ON_STATE;
+      digitalWrite(LED_PIN, ON_STATE ? HIGH : LOW);
+      fridge_state.publish(ON_STATE ? "HIGH" : "LOW");
     }
   }
 
@@ -175,10 +167,7 @@ void handleMQTT(float temp) {
   if (nowTime - lastPublish > 2000) {
     if (!fridge.publish(temp)) {
       Serial.println(F("Failed"));
-      delay(500);
-      digitalWrite(LED_PIN, LOW);
-      delay(500);
-      digitalWrite(LED_PIN, HIGH);
+      blink(500, 1);
     } else {
       Serial.println(F("OK!"));
     }
@@ -202,7 +191,7 @@ void MQTT_connect() {
     Serial.println(mqtt.connectErrorString(ret));
     Serial.println("Retrying MQTT connection in 5 seconds...");
     mqtt.disconnect();
-    delay(5000);  // wait 5 seconds
+    delay(500);  // wait 5 seconds
     retries--;
     if (retries == 0) {
       // basically die and wait for WDT to reset me
